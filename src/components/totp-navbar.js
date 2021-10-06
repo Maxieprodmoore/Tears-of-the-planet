efd6a34a0f2b46044a2b63d71c67fd8679c0619c
Maxieprodmoore <binglinho@gmail.com>
2021-10-06 10:50:45 +0800

    Added navbar and homepage

---
+++ src/components/totp-navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <a href="/" className="hmbtn"><img src="" alt="" /></a>
+            <div className="links">
+                <a href="/aboutus">About us</a>
+                <a href="/natureinDecline">Nature in decline</a>
+                <a href="/whybother">Why bother?</a>
+                <button>How can I help?</button>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;    
---
+++ src/views/pages/homepage.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return ( 
+        <div className="content">
+            <h1 className="intro">Presenting: Tears of the Planet</h1>
+            <h6>A condensation of the WWF and Living Planet report 2020 edition.</h6>
+        </div>
+        
+     );
+}
+ 
+export default Home;
