5f22b221e87e4b813e6a8ba277a82d675d898eeb
Maxieprodmoore <binglinho@gmail.com>
2021-12-05 19:49:11 +0800

    Added basic layer of information for call to action section.

--- src/views/pages/how-I-can-Help.js
+++ src/views/pages/how-I-can-Help.js
@@ -5,9 +5,80 @@ import  '@shoelace-style/react/dist/icon';
 const howICanHelp = () => {
     return (
         <div>
-          <h2>What can we do to save our planet?</h2>
-          <h3>Scientists are suggesting the following actions:</h3>
-          <h3>Helping our freshwater sources from our homes:</h3>
+          <div className="infoDump">
+            <h2>What can we do to save our planet?</h2>
+            <div className="info">
+              <p>
+                  We can start by rethinking our relationship with the planet 
+                  and finding the balance that will allow us and the rest of nature to survive.
+                </p>
+              
+                <p>
+                  Stop doing it altogether, and give up a product or activity. 
+                  But there needs to be an alternative to replace it with, which unfortunately could also cause problems.
+                </p>
+                <p>
+                  Do it less, so that nature is able to replace what we take or absorb the impact of our action. 
+                  <br />
+                  This could mean that we need to learn to be satisfied with less, or find ways to make the most of less.
+                  <br /> 
+                  Whether it be by wasting less food, or by reusing things rather than replacing them.
+                </p>
+                <p>
+                  Find new ways to do it so that we get the same benefit but in a way that does not cause damage to the planet. 
+                  <br />
+                  For instance, we can use renewable energy to power our electronic devices and heat our homes, rather than energy produced by burning fossil fuels.
+                </p>
+            </div>                          
+          </div>
+          <div className="infoDump">
+            <h3>Scientists are suggesting the following actions:</h3>
+            <div className="info">
+              <p>
+                Stop the wastage of food. 
+                <br />
+                Change some of our diets and change fishing practices to ensure that the oceans can thrive and replace what we take out.
+              </p>
+              <p>
+                If you feel bolder, choose a more plant-based diet.
+              </p>
+              <p>
+                Else, supporting food that is produced in a more environmentally sustainable manner,
+                <br /> 
+                and buying locally produced food (where possible) is a great start.
+              </p>
+              <p>
+                Check your energy is from 100% renewable sources and encourage the replacement to renewable sources.
+              </p>
+              <p>
+                Invest in ‘nature-based solutions’ that can support biodiversity while playing an active role in slowing climate change 
+                and protecting people and wildlife from its effects. 
+                <br />
+                <p> For example, carefully choosing places to plant more forests to strengthen landscapes.
+                    <br /> 
+                    Improve soil quality and capture carbon to help in the fight against climate change. 
+                </p>
+              </p>
+              <p>
+                Plant or support the planting of trees.
+                <br />
+                It can help improve air quality, prevent floods and keep residential areas cool.
+                <br /> 
+                and simply having trees nearby improves the physical and mental health of people living and working there.
+
+              </p>
+              <p>
+                Otherwise, just taking action for your local biodiversities go a long way in helping mother Earth.
+              </p>
+            </div>
+
+          </div>
+          <div className="infoDump">
+            <h3>
+              Don’t worry about taking action alone, there are peers out there who are just as eager to ensure Nature is healing:
+            </h3>
+          </div>
+          
           <h3>Share this with a friend/family member!</h3>
             <p></p>
             <div id="socials">
--- src/views/pages/nature-in-decline.js
+++ src/views/pages/nature-in-decline.js
@@ -5,7 +5,29 @@ import foodImg from './../../images/deforestationImg-Thumbnail.png';
 import seaImg from './../../images/dolphinsImg-Thumbnail.png';
 import freshWaterImg from './../../images/freshwaterDuckImg-Thumbnail.png';
 
-const detailsSoil = 'Soil is an essential part of the natural environment. Healthy soil filters water, help keep greenhouse gases from building up in the atmosphere and allow plants to grow – including our food. Where does the healthy soil come from? The decomposers within the soil are responsible for helping to break down dead plants and animals into that soil while other denizens are responsible for the incredible contribution soil makes to human well-being and the health of the rest of the planet. And even if they live on the surface, 90% of them live part of their life cycles in the soil. Thus, by understanding how and when our actions harm soil biodiversity, we can start making changes that help the soil’s inhabitants recover.'
+{'/*--------------------------Decline regarding Soil inhabitants------------------------*/'}
+const detailsSoil_header = 'Cries from the ground'
+const detailsSoil_Para1 = 'Soil is an essential part of the natural environment. Healthy soil filters water, help keep greenhouse gases from building up in the atmosphere and allow plants to grow – including our food.'
+const detailsSoil_Para2 = 'Where does the healthy soil come from? The decomposers within the soil are responsible for helping to break down dead plants and animals into that soil while other denizens are responsible for the incredible contribution soil makes to human well-being and the health of the rest of the planet. And even if they live on the surface, 90% of them live part of their life cycles in the soil.'
+const detailsSoil_Para3 = 'Thus, by understanding how and when our actions harm soil biodiversity, we can start making changes that help the soil’s inhabitants recover.'
+
+{'/*--------------------------Decline regarding Denizens of the Deep inhabitants------------------------*/'}
+const detailsSea_header = 'Cries of the Sea creatures'
+const detailsSea_Para1 = 'Overfishing, pollution, invasive species, deep-sea mining and increasing temperatures resulting from climate change are taking a devastating toll on the rich and varied wildlife of the ocean.'
+const detailsSea_Para2 = 'Healthy oceans filled with life help out with slowing down climate change and driving global weather systems, as well as providing livelihoods for more than 10% of the world’s population. For instance, Coral reefs, threatened by rising temperatures and coastal developments, provide vital protection from storm surges and waves for nearly 200 million people, as well as helping to support the entire ocean ecosystem.'
+const detailsSea_Para3 = 'As losing our ocean biodiversity affects every living thing on the planet, we need Marine Protected Areas (MPAs) to create wild spaces and restore the ocean’s natural balance. We also need to end plastic pollution and destructive fishing practices.'
+
+{'/*--------------------------Decline regarding freshWater inhabitants------------------------*/'}
+const detailsFreshWater_header = 'Freshwater woes'
+const detailsFreshWater_Para1 = 'Freshwater wildlife populations have declined by 84% on average from 1970s onwards. Almost one in three freshwater species are threatened with extinction, with larger animals such as hippos, river dolphins, sturgeon and beavers being generally the most at risk.'
+const detailsFreshWater_Para2 = 'Despite how important water is for life and health, freshwater ecosystems are sadly the most threatened on Earth, with 90% of wetlands being lost since the 1700s and the altering of river flows being major factors for that. But if we help to protect critical wetland habitats and end overfishing, we would be on a great start in helping freshwater habitats recover.'
+const detailsFreshWater_Para3 = ''
+
+{'/*--------------------------Decline regarding Denizens of the Deep inhabitants------------------------*/'}
+const detailsFood_header = 'Food woes'
+const detailsFood_Para1 = '1 third of Earth’s land is dedicated to farming crops and livestock, accounting for 75% of our freshwater usage. Unfortunately, out of all the food we produce, ⅓ of it is never eaten.'
+const detailsFood_Para2 = 'This is an issue as Food waste is responsible for roughly 8% of greenhouse gases added to the atmosphere, including gases released via decomposition.'
+const detailsFood_Para3 = ''
 
 const natureInDecline = () => {
     return (  
@@ -31,9 +53,10 @@ const natureInDecline = () => {
                         </p>
                     </div>
                     <div className="infoDetails">
-                        <p>90% of land dwellers spend part of their life cycle in the soil.</p>
-                        <h3>The life under our feet</h3>
-                        <p>Soil is an essential part of the natural environment.</p>
+                        <h3>{detailsSoil_header}</h3>
+                        <p>{detailsSoil_Para1}</p>
+                        <p>{detailsSoil_Para2}</p>
+                        <p>{detailsSoil_Para3}</p>
                     </div>
                 </div>
                 
