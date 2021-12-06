997865556cb6043d254bf716d320a66d07006f7a
Maxieprodmoore <binglinho@gmail.com>
2021-12-06 18:11:45 +0800

    Changing navbar for quick Accessing

--- src/components/totp-navbar.js
+++ src/components/totp-navbar.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import Logo from './../images/WWF_Logo_Horizontal.png';
 
 const Navbar = () => {
+
     return ( 
         <nav className="navbar">
             <a href="/" className="hmbtn"><img src={Logo} alt="Home button" /></a>
@@ -10,7 +11,10 @@ const Navbar = () => {
                 <Link to="/about-us">About us</Link>
                 <Link to="/nature-in-decline">Nature in decline</Link>
                 <Link to="/why-bother">Why bother?</Link>
-                <SLButton type="success" href="/how-i-can-help">How can I help?</SLButton>
+                <Link to="/how-i-can-help">
+                    <SLButton type="success" >How can I help?</SLButton>
+                </Link>
+                
             </div>
         </nav>
      );
