743732f453104ceb86153bfc7c56f73bf1163923
Maxieprodmoore <binglinho@gmail.com>
2021-12-06 15:26:23 +0800

    reordered import list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import '@shoelace-style/react/dist/button';
 import '@shoelace-style/react/dist/button-group';
 import '@shoelace-style/shoelace/dist/themes/light.css';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.61/dist/');
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './../src/components/totp-navbar';
 import Home from './../src/views/pages/homepage';
@@ -10,6 +8,8 @@ import AboutUs from './../src/views/pages/about-us';
 import NatureInDecline from './../src/views/pages/nature-in-decline';
 import WhyBother from './../src/views/pages/why-bother';
 import HowICanHelp from './../src/views/pages/how-I-can-Help';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.61/dist/');
 
 function App() {
   return (
