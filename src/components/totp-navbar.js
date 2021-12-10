de42fd227d3af926c4f095205bb0e5e6fab8ea2b
Maxieprodmoore <binglinho@gmail.com>
2021-12-10 15:56:24 +0800

    Created hamburger menu for responsive design

--- src/components/totp-navbar.js
+++ src/components/totp-navbar.js
@@ -1,31 +1,48 @@
+import { useState } from 'react';
 import SlButton from '@shoelace-style/react/dist/button';
+import SlIconButton from '@shoelace-style/react/dist/icon-button';
+import SlDrawer from '@shoelace-style/react/dist/drawer';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from './../images/WWF_Logo_Vertical.svg';
 
 const Navbar = () => {
 
+    const [open, setOpen] = useState(false);
+
     return ( 
-        <nav className="navbar">
-            <Logo className="hmbtn" onClick={(e) => {
-                e.preventDefault();
-                window.location.href='/';
-            }}/>
-            <div className="links">
-                <div>
+        <div >
+            <nav className="navbar">
+                <Logo className="hmbtn" onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href='/';
+                }}/>
+                <div className="links">
                     <Link to="/about-this-page">About this page</Link>
-                    <Link to="/nature-in-decline">Nature in decline</Link>
-                </div>
-                
-                <div>
+                    <Link to="/nature-in-decline">Nature in decline</Link>                    
                     <Link to="/why-it-matters">Why it matters</Link>
                     <Link to="/how-i-can-help">
                         <SlButton type="success" >How can I help?</SlButton>
                     </Link>
+                                
                 </div>
-                               
-            </div>
-        </nav>
-     );
+                <SlIconButton id="Hmburger" name="list" onClick={() => setOpen(true)}/>
+            </nav>
+            <SlDrawer open={open} onSlAfterHide={() => setOpen(false)} id="HmburgerDrawer">
+                <Link to="/about-this-page">About this page</Link>
+                <Link to="/nature-in-decline">Nature in decline</Link>
+                <Link to="/why-it-matters">Why it matters</Link>
+                <Link to="/how-i-can-help">
+                    <SlButton type="success" >How can I help?</SlButton>
+                </Link>
+                <SlButton slot="footer" type="primary" onClick={() => setOpen(false)}>
+                    Close
+                </SlButton>
+            </SlDrawer>
+        </div>
+        
+        
+
+    );
 }
  
 export default Navbar;    
